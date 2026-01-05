68f6d5d56300f0c4d60218c7dcd0fdd7c408abbd
Abdullahx-z <156074235+Abdullahx-z@users.noreply.github.com>
2026-01-05 14:45:46 +0100

    fetch v1

---
+++ src/main/scala/Fetch.scala
@@ -0,0 +1,36 @@
+import chisel3._
+import chisel3.util._
+
+class Fetch() extends Module {
+    val io = IO(new Bundle {
+        val pc = Output(UInt(32.W))
+        val instr = Output(UInt(32.W))
+    })
+
+   val pcReg = RegInit(0.U(32.W))
+
+   pcReg = pcReg + 4.U
+    
+   io.pc := pcReg
+   //hardcoded instruction fra git 0x12300093
+   io.instr := "h12300093".U(32.W)
+}
+
+class IF_reg_ID() extends Module {
+    val io = IO(new Bundle{
+        val pcIn = Input(UInt(32.W))
+        val instrIn = Input(UInt(32.W))
+
+        val pcRegOut = Output(UInt(32.W))
+        val instrRegOut = Output(UInt(32.W))
+    })
+
+    val pcReg = RegInit(0(32.W))
+    val instrReg = RegInit(0(32.W))
+
+    pcRegsaved = io.pcIn
+    instrRegsaved = io.instrIn
+
+    io.pcRegOut := pcReg
+    io.instrRegOut := instrRegsaved
+}
