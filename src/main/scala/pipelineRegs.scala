238cad8110c6f277d0c47130e545d931ae6e1513
Abdullahx-z <156074235+Abdullahx-z@users.noreply.github.com>
2026-01-06 14:09:33 +0100

    ID_reg_EX

--- src/main/scala/pipelineRegs.scala
+++ src/main/scala/pipelineRegs.scala
@@ -22,8 +22,29 @@ class IF_ID extends Module {
 
 class ID_reg_EX extends Module {
   val io = IO(new Bundle{
-    
+        val rs1In = Input(UInt(8.W))
+        val rs2In = Input(UInt(8.W))
+        val isADDIn = Input(Bool())
+        val pcIn = Input(UInt(32.W))
+
+        val rs1Out = Output(UInt(8.W))
+        val rs2Out = Output(UInt(8.W))
+        val isADDOut = Output(Bool())
+        val pcOut = Output(UInt(32.W))
   })
 
+    val rs1Reg = RegInit(0.U(8.W))
+    val rs2Reg = NextReg(0.U(8.W))
+    val isAddReg = NextReg(Bool())
+    val pcInReg = RegInit(0.U((32.W)))
 
+    rs1Reg = io.rs1In
+    rs2Reg = io.rs2In
+    isAddReg = io.isADDIn
+    pcInReg = io.pcIn
+
+    io.rs1Out := rs1Reg
+    io.rs2Out := rs2Reg
+    io.isADDOut := isAddReg
+    io.pcOut := pcInReg
 }
