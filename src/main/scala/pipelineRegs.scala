df6b2c9531bd9d3bce0ba8dde65582c9eb254521
Abdullahx-z <156074235+Abdullahx-z@users.noreply.github.com>
2026-01-06 13:20:51 +0100

    fetch reg

---
+++ src/main/scala/pipelineRegs.scala
@@ -0,0 +1,25 @@
+import chisel3._
+import chisel3.util._
+
+class IF_reg_ID extends Module {
+  new io = IO(new Bundle{
+    val pc = Input(UInt(32.W))
+    val instr = Input(UInt(32.W))
+    
+    val pcRegOut = Output(UInt(32.W))
+    val pc4RegOut = Output(UInt(32.W))
+    val instrRegOut = Output(UInt(32.W))
+  })
+
+  val pcReg = RegInit(UInt(0.U(32.W)))
+  val pc4Reg = RegInit(UInt(0.U(32.W)))
+  val instrReg = RegInit(UInt(0.U(32.W)))
+
+  pcReg = io.pc
+  pc4Reg = io.pc + 4.U
+  instrReg = io.instr
+
+  io.pcRegOut := pcReg
+  io.pc4RegOut := pc4Reg
+  io.instrRegOut := instrReg
+}
