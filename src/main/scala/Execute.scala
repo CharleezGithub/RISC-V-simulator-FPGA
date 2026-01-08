324cbcc40358709c2a6f1516f7c84b74fd1a83be
alex255c <alexandergrewal04@gmail.com>
2026-01-08 12:56:48 +0100

    Update Execute.scala

--- src/main/scala/Execute.scala
+++ src/main/scala/Execute.scala
@@ -3,25 +3,77 @@ import chisel3.util._
 
 class Execute extends Module {
     val io = IO(new Bundle{
-        val rs1In = Input(UInt(32.W))
-        val rs2In = Input(UInt(32.W))
-        val isADDIn = Input(Bool())
+        // Inputs for operation selection
+        val funct3 = Input(UInt(32.W))
+        val funct7 = Input(UInt(32.W))
+        val opcode = Input(UInt(32.W))
+
+        // Data values
+        val rs1Data = Input(UInt(32.W))
+        val rs2Data = Input(UInt(32.W))
+        val imm = Input(UInt(32.W))
+
         val pcIn = Input(UInt(32.W))
 
+        // Outputs
         val ALUout = Output(UInt(32.W))
         val pcOut = Output(UInt(32.W))
     })
 
     // Passing on relevant values
     io.pcOut := io.pcIn
+    io.ALUout := 0.U // Init value
+    
 
-    io.ALUout := RegInit(11111.U(32.W))
-    
-    when(io.isADDIn === true.B){
-        io.ALUout := io.rs1In + io.rs2In
-    } 
-    
+    switch(io.opcode) {
+        //---------------------------------------------(   R-type   )-----------------------------------------------------
+        is("b0110011".U) { 
+            switch(io.funct7) {
+                is("b0000000".U) { 
+                    switch(io.funct3) {
+                        is("b000".U) { io.ALUout := io.rs1Data + io.rs2Data }                                       // ADD
+                        is("b111".U) { io.ALUout := io.rs1Data & io.rs2Data }                                       // AND
+                        is("b110".U) { io.ALUout := io.rs1Data | io.rs2Data }                                       // OR
+                        is("b100".U) { /* XOR */ }                                                                  // XOR
+                        is("b001".U) { /* SLL */ }                                                                  // SLL
+                        is("b101".U) { /* SRL */ }                                                                  // SRL
+                        is("b010".U) { io.ALUout := (io.rs1Data.asSInt < io.rs2Data.asSInt).asUInt }                // SLT
+                        is("b011".U) { /* SLTU */ }                                                                 // SLTU
+                    }
+                }
+                is("b0100000".U) { 
+                    switch(io.funct3) {
+                        is("b000".U) { io.ALUout := io.rs1Data - io.rs2Data }                                       // SUB
+                        is("b101".U) { /* SRA */ }                                                                  // SRA
+                    }
+                }
+            }
+        }
+
+        //---------------------------------------------(   I-type   )-----------------------------------------------------
+        is("b0010011".U) { // I-type ALU
+            switch(io.funct3) {
+                is("b000".U) { io.ALUout := io.rs1Data + io.imm }                                                   // ADDI
+                is("b111".U) { io.ALUout := io.rs1Data & io.imm }                                                   // ANDI
+                is("b110".U) { io.ALUout := io.rs1Data | io.imm }                                                   // ORI
+                is("b100".U) { /* XORI */ }                                                                         // XORI
+                is("b010".U) { /* SLTI */ }                                                                         // SLTI
+                is("b011".U) { /* SLTIU */ }                                                                        // SLTIU
+                is("b001".U) { /* SLLI */ }    
+           
+                is("b101".U) { 
+                    switch(io.funct7) {
+                        is("b0000000".U) { /* SRLI */ }
+                        is("b0100000".U) { /* SRAI */ }
+                    }
+                }
+            }
+        }
+    }
+
 }
+  
+
 object Execute extends App {
   emitVerilog(new Execute())
 }
