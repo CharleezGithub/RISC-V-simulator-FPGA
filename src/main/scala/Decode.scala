726a1b29b1be5cbbb337b318cc50d96dff395bfc
Abdullahx-z <156074235+Abdullahx-z@users.noreply.github.com>
2026-01-09 12:26:56 +0100

    control unit

--- src/main/scala/Decode.scala
+++ src/main/scala/Decode.scala
@@ -6,7 +6,7 @@ class Decode extends Module {
         val pcIn = Input(UInt(32.W))
         val instrIn = Input(UInt(32.W))
 
-        // inputs for reg writes
+        // Control unit inputs
         val writeData = Input(UInt(32.W))
         val writeFlag = Input(Bool())
         val writeAddr = Input(UInt(5.W))
@@ -24,6 +24,14 @@ class Decode extends Module {
         val immUOut = Output(UInt(32.W))
         val immJOut = Output(UInt(32.W))
         val rdAddrOut = Output(UInt(8.W))
+
+        //Control unit outputs
+        val widthSizeOut = Output(UInt(2.W)) // selects mem access size (00=byte, 01=half, 10=word)
+        val memWriteOut = Output(Bool()) // enables writ to data mem
+        val memReadOut = Output(Bool()) // enables read from data mem
+        val wbFlagOut = Output(Bool()) // enables writing a val to reg
+        val wbALUOrMemOut = Output(Bool())  // selects where to write back, whether to a reg (ALU = 0) or to mem (MEM = 1)
+        
     })
 
     val instr = io.instrIn
@@ -87,6 +95,113 @@ class Decode extends Module {
     io.immJOut := immJ
     io.immSOut := immS
     io.immUOut := immU
+
+    
+    // Control unit
+    io.memReadOut := false.B
+    io.memWriteOut := false.B
+    io.wbFlagOut := false.B
+    io.wbALUOrMemOut := false.B
+    io.widthSizeOut := "b00".U
+    switch(opcode){
+        //load word sets
+        is( "b0000011".U){
+            switch(funct3){
+                //LB
+                is("b000".U){
+                    io.memReadOut := true.B
+                    io.memWriteOut := false.B
+                    io.wbFlagOut := true.B
+                    io.wbALUOrMemOut := true.B
+                    io.widthSizeOut := "b0".U
+                }
+                //LH
+                is("b001".U){
+                    io.memReadOut := true.B
+                    io.memWriteOut := false.B
+                    io.wbFlagOut := true.B
+                    io.wbALUOrMemOut := true.B
+                    io.widthSizeOut := "b01".U
+                }
+                //LW
+                is("b010".U){
+                    io.memReadOut := true.B
+                    io.memWriteOut := false.B
+                    io.wbFlagOut := true.B
+                    io.wbALUOrMemOut := true.B
+                    io.widthSizeOut := "b00".U
+                }
+                //LBU
+                is("b100".U){
+                    io.memReadOut := true.B
+                    io.memWriteOut := false.B
+                    io.wbFlagOut := true.B
+                    io.wbALUOrMemOut := true.B
+                    io.widthSizeOut := "b00".U                    
+                }
+                //LHU
+                is("b101".U){
+                    io.memReadOut := true.B
+                    io.memWriteOut := false.B
+                    io.wbFlagOut := true.B
+                    io.wbALUOrMemOut := true.B
+                    io.widthSizeOut :=  "b01".U
+                }
+            }
+        }
+        is("b0100011".U){
+            switch(funct3){
+                //SB
+                is("b000".U){
+                    io.memReadOut := false.B
+                    io.memWriteOut := true.B
+                    io.wbFlagOut := false.B
+                    io.wbALUOrMemOut := false.B
+                    io.widthSizeOut := "b00".U
+                }
+                //SH
+                is("b001".U){
+                    io.memReadOut := false.B
+                    io.memWriteOut := true.B
+                    io.wbFlagOut := false.B
+                    io.wbALUOrMemOut := false.B
+                    io.widthSizeOut := "b01".U
+                }
+                //SW
+                is("b010".U){
+                    io.memReadOut := false.B
+                    io.memWriteOut := true.B
+                    io.wbFlagOut := false.B
+                    io.wbALUOrMemOut := false.B
+                    io.widthSizeOut := "b10".U
+                }
+            }
+        }
+        //Jumps, ALU-I, ALu-reg, 
+        is("b0110011".U, "b0010011".U, "b1101111".U, "b1100111".U){
+                io.memReadOut := false.B
+                io.memWriteOut := false.B
+                io.wbFlagOut := true.B
+                io.wbALUOrMemOut := false.B   
+        }
+        //branch
+        is("b1100011".U){
+                io.memReadOut := false.B
+                io.memWriteOut := false.B
+                io.wbFlagOut := false.B
+                io.wbALUOrMemOut := false.B               
+        }
+        //upper imm
+        is("b0110111".U, "b0010111".U){
+            io.memReadOut := false.B
+            io.memWriteOut := false.B
+            io.wbFlagOut := true.B
+            io.wbALUOrMemOut := false.B   
+        }
+    }
+
+
+
 }
 object Decode extends App {
     emitVerilog(new Decode())
