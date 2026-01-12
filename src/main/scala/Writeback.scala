28fbacd6f9620ee745cb240604f840bea45f6db4
alex255c <alexandergrewal04@gmail.com>
2026-01-12 12:20:56 +0100

    Update Writeback.scala

--- src/main/scala/Writeback.scala
+++ src/main/scala/Writeback.scala
@@ -7,22 +7,26 @@ class Writeback extends Module {
         val ALUIn = Input(UInt(32.W))
         val rdAddr = Input(UInt(5.W))
         val rdDataIn = Input(UInt(32.W))
-        val regWrite = Input(Bool())
 
         // To register file
         val rfWAddr = Output(UInt(5.W))
         val rfWData = Output(UInt(32.W))
-        val rfWEn = Output(Bool())
 
         val widthSizeIn = Input(UInt(2.W))
         val memWriteIn = Input(Bool())
         val memReadIn = Input(Bool())
         val wbFlagIn = Input(Bool())
-        val wbALUOrMemIn = Input(Bool())
+        val wbALUOrMemIn = Input(Bool()) // rfEnable
+
+        // Output to reg file
+        val rfWEn = Output(Bool())
     })
     // Data will be sent to certain address in register file when enable signal true
     io.rfData := io.ALUin
     io.rfWAddr := io.rdAddr
+
+    // If we need to save ALU output to register file rfWEn is true
+    io.rfWEn := Mux(io.wbALUOrMemIn, false.B, true.B)
 
 }
 
